7c04397a816d7a78847e94bd3baa27540b30ad30
Prateek Verghese <prateek@2Prateeks-MacBook-Pro.local>
2023-02-05 06:32:13 -0800

    mvc added

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,62 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            return User.findOne({ _id: context.user._id })
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No user with this email found!');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, { bookInput }, context) => {
+            console.log("User", context.user)
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: bookInput } },
+                    { new: true }
+                );
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                );
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    }
+}
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+}
+
+type  Auth {
+    token: ID!
+    user: User
+}
+
+
+input BookInput{
+    bookId: String!
+    authors: [String]
+    description: String 
+    image: String
+    title: String
+}
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookInput : BookInput!) : User
+    removeBook(bookId: String!) : User
+}
+`
+module.exports = typeDefs;
+
+// Setting an Auth tupe to handle returning data from user creating or use login
